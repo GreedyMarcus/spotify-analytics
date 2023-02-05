09105108d25fb14938e48aad464df42d40008e0c
Mark Domahidi <mdomahidi@gmail.com>
2023-02-05 12:23:14 +0100

    fix: remove unnecessary code

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,26 +1,15 @@
 import { RefObject, useEffect } from "react";
-import { DomUtils } from "@utils/dom.utils";
 
 export const useClickOutside = (ref: RefObject<HTMLElement>, callback: VoidFunction) => {
   useEffect(() => {
-    const shouldIgnore = (target: HTMLElement): boolean => {
-      if (DomUtils.parseDataAttribute(target?.dataset.ignoreClickOutside)) {
-        return true;
-      } else {
-        return target?.parentElement ? shouldIgnore(target.parentElement) : false;
-      }
-    };
-
     const handler = (e: MouseEvent | TouchEvent) => {
       e.stopPropagation();
 
-      const target = e.target as HTMLElement;
-
-      if (ref.current && ref.current !== target && !ref.current.contains(target)) {
-        if (!shouldIgnore(target)) {
-          callback();
-        }
+      if (!ref.current || ref.current.contains(e.target as HTMLElement)) {
+        return;
       }
+
+      callback();
     };
 
     document.addEventListener("mousedown", handler);
@@ -30,5 +19,5 @@ export const useClickOutside = (ref: RefObject<HTMLElement>, callback: VoidFunct
       document.removeEventListener("mousedown", handler);
       document.removeEventListener("touchstart", handler);
     };
-  }, [callback]);
+  }, [ref, callback]);
 };
